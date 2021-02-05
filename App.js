9ff02a5e2742117ff3f4b71eba7dc4e99d3e4795
pariwesh009 <ajay2322020@outlook.com>
2021-02-06 01:15:00 +0530

    introdcude state management

--- App.js
+++ App.js
@@ -1,16 +1,73 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TextInput } from 'react-native';
 
-export default function App() {
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => {
+  // let state = { value: 'test' }
+  const [state, onChangeText] = useState({name:' Placeholder',age:'10'});
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
-}
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Name:
+      </Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => onChangeText({name:text})}
+          value={state.name}
+        />
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => onChangeText({age:text})}
+          value={state.age}
+        />
+        <Button
+          title="save"
+          onPress={() => {
+            alert(state.age);
+            console.log('test')
+          }}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
+      </Text>
+        <Button
+          title="Press me"
+          color="#f194ff"
+          onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
+      </View>
+      <Separator />
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default App;
